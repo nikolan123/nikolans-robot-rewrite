141a58c3c2f5f54da8de1540e02d74148eb8a42f
Restart <79792966+RestartB@users.noreply.github.com>
2024-06-08 17:50:40 +0100

    feat: :sparkles: Add Random CPU and Random GPU commands

--- cogs/apicmds.py
+++ cogs/apicmds.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import aiohttp
 import asyncio
 from discord.ext.commands import BucketType
+import random
 
 class apicmds(commands.Cog):
     def __init__(self, bot):
@@ -29,6 +30,64 @@ class apicmds(commands.Cog):
             embed.color = discord.Colour.red()
             await ctx.respond(embed=embed)
 
+    @commands.slash_command(name="random-cpu", description="Sends a random Intel or AMD CPU.")
+    @commands.cooldown(1, 3, BucketType.user)
+    async def random_cpu(self, ctx):
+        await ctx.defer(ephemeral=False)
+        
+        if random.choice(["intel", "amd"]) == "intel":
+            # Get data from API
+            endpoint = "https://api.nikolan.xyz/intel-cpu"
+            data = await self.fetch_data(ctx, endpoint)
+            
+            # Process data
+            if data:
+                cpu_info = data.get("cpu")
+                if cpu_info:
+                    ghzs = f"**Base Frequency** {cpu_info['Base Freq.(GHz)']}GHz" if cpu_info['Max. Turbo Freq.(GHz)'] == "N/A" else f"**Base/Max Frequency** {cpu_info['Base Freq.(GHz)']}/{cpu_info['Max. Turbo Freq.(GHz)']}GHz"
+                    embed = discord.Embed(title=f"Intel {cpu_info['Product']}", color=0x2494A1, description=f"**Cores/Threads** {cpu_info['Cores']}c{cpu_info['Threads']}t\n**Release Date** {cpu_info['Release Date']}\n**TDP** {cpu_info['TDP(W)']}W\n**Lithography** {cpu_info['Lithography(nm)']}nm\n{ghzs}")
+                    embed.set_footer(text=f"Requested by {ctx.author.name} | api.nikolan.xyz/intel-cpu")
+                    await ctx.respond(embed=embed)
+                else:
+                    embed = discord.Embed(title = "Error", description = f"An error occurred while fetching CPU data from the API. Try again later.")
+                    embed.color = discord.Colour.red()
+                    await ctx.respond(embed=embed)
+        else:
+            # Get data from API
+            endpoint = "https://api.nikolan.xyz/amd-cpu"
+            cpu_info = await self.fetch_data(ctx, endpoint)
+            
+            # Process data
+            if cpu_info:
+                stupid = "\u00c2\u00b9 \u00c2\u00b2"
+                cpumodel = cpu_info['Model']
+                clockbase = cpu_info['Base Clock'].replace('GHz', '')
+                clockmax = cpu_info[f'Max. Boost Clock {stupid}'].replace("GHz", "").replace("Up to ", "")
+                rdate = cpu_info['Launch Date']
+                if rdate == "":
+                    rdate = "Unknown"
+                if not "AMD" in cpumodel:
+                    cpumodel = f"AMD {cpumodel}"
+                tdp = cpu_info['Default TDP']
+                if tdp == "":
+                    tdp = "Unknown"
+                cpumodel = cpumodel.replace("â„¢", "")
+                embed = discord.Embed(
+                    title=f"{cpumodel}",
+                    color=0xff0000,
+                    description=f"""**Cores/Threads** {cpu_info['# of CPU Cores']}c{cpu_info['# of Threads']}t
+                **Release Date** {rdate}
+                **TDP** {tdp}
+                **Lithography** {cpu_info['Processor Technology for CPU Cores']}
+                **Base/Max Frequency** {clockbase}/{clockmax}GHz""")
+
+                embed.set_footer(text=f"Requested by {ctx.author.name} | api.nikolan.xyz/amd-cpu")
+                await ctx.respond(embed=embed)
+            else:
+                embed = discord.Embed(title = "Error", description = f"An error occurred while fetching CPU data from the API. Try again later.")
+                embed.color = discord.Colour.red()
+                await ctx.respond(embed=embed)
+    
     @commands.slash_command(name="intel-cpu", description="Sends a random Intel CPU.")
     @commands.cooldown(1, 3, BucketType.user)
     async def cpur(self, ctx):
@@ -89,6 +148,55 @@ class apicmds(commands.Cog):
             embed.color = discord.Colour.red()
             await ctx.respond(embed=embed)
 
+    @commands.slash_command(name="random-gpu", description="Sends a random Nvidia or AMD GPU.")
+    @commands.cooldown(1, 3, BucketType.user)
+    async def random_cpu(self, ctx):
+        await ctx.defer(ephemeral=False)
+
+        if random.choice(["nvidia", "amd"]) == "nvidia":
+            # Get data from API
+            endpoint = "https://api.nikolan.xyz/nvidia-gpu"
+            data = await self.fetch_data(ctx, endpoint)
+            
+            # Process data
+            if data:
+                gpuname = data["gpu"]
+                if not "nvidia" in gpuname.lower():
+                    gpuname = f"Nvidia {gpuname}"
+                embed = discord.Embed(title=gpuname, color=0x00ff00)
+                embed.set_footer(text=f"Requested by {ctx.author.name} | api.nikolan.xyz/nvidia-gpu")
+                await ctx.respond(embed=embed)
+            else:
+                embed = discord.Embed(title = "Error", description = f"An error occurred while fetching GPU data from the API. Try again later.")
+                embed.color = discord.Colour.red()
+                await ctx.respond(embed=embed)
+        else:
+            # Get data from API
+            endpoint = "https://api.nikolan.xyz/amd-gpu"
+            cpu_info = await self.fetch_data(ctx, endpoint)
+            
+            # Process data
+            if cpu_info:
+                cpumodel = cpu_info['Model']
+                rdate = cpu_info['Launch Date']
+                tdp = cpu_info['Typical Board Power (Desktop)']
+                maxvram = cpu_info['Max Memory Size']
+                ramtype = cpu_info['Memory Type']
+                embed = discord.Embed(
+                    title=f"{cpumodel}",
+                    color=0xff0000,
+                    description=f"""
+                **Release Date** {rdate}
+                **VRAM**: Up to {maxvram} {ramtype}
+                **TBP** {tdp}
+                """)
+                embed.set_footer(text=f"Requested by {ctx.author.name} | api.nikolan.xyz/amd-gpu")
+                await ctx.respond(embed=embed)
+            else:
+                embed = discord.Embed(title = "Error", description = f"An error occurred while fetching GPU data from the API. Try again later.")
+                embed.color = discord.Colour.red()
+                await ctx.respond(embed=embed)
+    
     @commands.slash_command(name="amd-gpu", description="Sends a random AMD GPU.")
     @commands.cooldown(1, 3, BucketType.user)
     async def gpumyy(self, ctx):
