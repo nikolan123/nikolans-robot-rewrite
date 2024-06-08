e4cc01c8204f39835a23501a899d4b78e10f0fa8
Restart <79792966+RestartB@users.noreply.github.com>
2024-06-08 15:37:26 +0100

    feat: :sparkles: Add Thank You embed to suggestions commands

--- cogs/suggestions.py
+++ cogs/suggestions.py
@@ -3,28 +3,33 @@ from discord.ext import commands
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
 
-class suhestyons(commands.Cog):
+class suggestions(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
 
     @commands.slash_command(name="suggest", description="Suggest a feature to the bot's developers.")
-    async def sugesttt(self, ctx, the: discord.Option(str, name="suggestion", description="Your suggestion")): # type: ignore
+    async def suggestion(self, ctx, the: discord.Option(str, name="suggestion", description="Your suggestion")): # type: ignore
         rlhook = DiscordWebhook(url=self.bot.suggestionshook, username=f"{self.bot.logginghookname} - suggestions")
         rlembed = DiscordEmbed(title="New suggestion!", description=f"User {ctx.author.name} ({ctx.author.id}):\n\n>>> {the}", color="7adedb")
         rlembed.set_timestamp()
         rlhook.add_embed(rlembed)
         rlhook.execute()    
-        await ctx.respond("Thank you!", ephemeral=True)
+        
+        embed = discord.Embed(title = "Thank you!", description = f"We have recieved your suggestion.")
+        embed.color = discord.Colour.green()
+        await ctx.respond(embed=embed, ephemeral=True)
     @commands.slash_command(name="bugreport", description="Report a bug to the bot's developers.")
-    async def reeeeeefwrgfszfdg(self, ctx, the: discord.Option(str, name="bug", description="Describe the bug you found")): # type: ignore
+    async def bugreport(self, ctx, the: discord.Option(str, name="bug", description="Describe the bug you found")): # type: ignore
         rlhook = DiscordWebhook(url=self.bot.suggestionshook, username=f"{self.bot.logginghookname} - bug report")
         rlembed = DiscordEmbed(title="New bug report!", description=f"User {ctx.author.name} ({ctx.author.id}):\n\n>>> {the}", color="7adedb")
         rlembed.set_timestamp()
         rlhook.add_embed(rlembed)
         rlhook.execute()    
-        await ctx.respond("Thank you!", ephemeral=True)
 
+        embed = discord.Embed(title = "Thank you!", description = f"We have recieved your bug report.")
+        embed.color = discord.Colour.green()
+        await ctx.respond(embed=embed, ephemeral=True)
 
 def setup(bot):
-    bot.add_cog(suhestyons(bot))
+    bot.add_cog(suggestions(bot))
