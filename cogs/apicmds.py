9fd5f9353ef2ebbaa97fb1255d7d0e91a37bbff8
nikolan <94351312+nikolan123@users.noreply.github.com>
2024-07-23 21:29:41 +0300

    improve random cpu

--- cogs/apicmds.py
+++ cogs/apicmds.py
@@ -30,72 +30,27 @@ class apicmds(commands.Cog):
             embed.color = discord.Colour.red()
             await ctx.respond(embed=embed)
 
-    @commands.slash_command(name="random-cpu", description="Sends a random Intel or AMD CPU.")
-    @commands.cooldown(1, 3, BucketType.user)
-    async def random_cpu(self, ctx):
-        await ctx.defer(ephemeral=False)
-        
-        if random.choice(["intel", "amd"]) == "intel":
-            # Get data from API
-            endpoint = "https://api.nikolan.xyz/intel-cpu"
-            data = await self.fetch_data(ctx, endpoint)
-            
-            # Process data
-            if data:
-                cpu_info = data.get("cpu")
-                if cpu_info:
-                    ghzs = f"**Base Frequency** {cpu_info['Base Freq.(GHz)']}GHz" if cpu_info['Max. Turbo Freq.(GHz)'] == "N/A" else f"**Base/Max Frequency** {cpu_info['Base Freq.(GHz)']}/{cpu_info['Max. Turbo Freq.(GHz)']}GHz"
-                    embed = discord.Embed(title=f"Intel {cpu_info['Product']}", color=0x2494A1, description=f"**Cores/Threads** {cpu_info['Cores']}c{cpu_info['Threads']}t\n**Release Date** {cpu_info['Release Date']}\n**TDP** {cpu_info['TDP(W)']}W\n**Lithography** {cpu_info['Lithography(nm)']}nm\n{ghzs}")
-                    embed.set_footer(text=f"Requested by {ctx.author.name} | api.nikolan.xyz/intel-cpu")
-                    await ctx.respond(embed=embed)
-                else:
-                    embed = discord.Embed(title = "Error", description = f"An error occurred while fetching CPU data from the API. Try again later.")
-                    embed.color = discord.Colour.red()
-                    await ctx.respond(embed=embed)
-        else:
-            # Get data from API
-            endpoint = "https://api.nikolan.xyz/amd-cpu"
-            cpu_info = await self.fetch_data(ctx, endpoint)
-            
-            # Process data
-            if cpu_info:
-                stupid = "\u00c2\u00b9 \u00c2\u00b2"
-                cpumodel = cpu_info['Model']
-                clockbase = cpu_info['Base Clock'].replace('GHz', '')
-                clockmax = cpu_info[f'Max. Boost Clock {stupid}'].replace("GHz", "").replace("Up to ", "")
-                rdate = cpu_info['Launch Date']
-                if rdate == "":
-                    rdate = "Unknown"
-                if not "AMD" in cpumodel:
-                    cpumodel = f"AMD {cpumodel}"
-                tdp = cpu_info['Default TDP']
-                if tdp == "":
-                    tdp = "Unknown"
-                cpumodel = cpumodel.replace("â„¢", "")
-                embed = discord.Embed(
-                    title=f"{cpumodel}",
-                    color=0xff0000,
-                    description=f"""**Cores/Threads** {cpu_info['# of CPU Cores']}c{cpu_info['# of Threads']}t
-                **Release Date** {rdate}
-                **TDP** {tdp}
-                **Lithography** {cpu_info['Processor Technology for CPU Cores']}
-                **Base/Max Frequency** {clockbase}/{clockmax}GHz""")
-
-                embed.set_footer(text=f"Requested by {ctx.author.name} | api.nikolan.xyz/amd-cpu")
-                await ctx.respond(embed=embed)
-            else:
-                embed = discord.Embed(title = "Error", description = f"An error occurred while fetching CPU data from the API. Try again later.")
-                embed.color = discord.Colour.red()
-                await ctx.respond(embed=embed)
-    
-    @commands.slash_command(name="intel-cpu", description="Sends a random Intel CPU.")
-    @commands.cooldown(1, 3, BucketType.user)
-    async def cpur(self, ctx):
-        await ctx.defer(ephemeral=False)
-        
+    async def intelcpu(self, ctxauthor):
         # Get data from API
         endpoint = "https://api.nikolan.xyz/intel-cpu"
-        data = await self.fetch_data(ctx, endpoint)
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(endpoint, timeout=3) as response:
+                    response.raise_for_status()
+                    if response.status == 200:
+                        data = await response.json()
+        except aiohttp.ClientError:
+            embed = discord.Embed(title = "Error", description = f"An error occurred while fetching data from the API. Please contact @{self.bot.ownername}.")
+            embed.color = discord.Colour.red()
+            return embed
+        except asyncio.TimeoutError:
+            embed = discord.Embed(title = "Error", description = f"The request to the API timed out. Please contact @{self.bot.ownername}.")
+            embed.color = discord.Colour.red()
+            return embed
+        except Exception:
+                embed = discord.Embed(title = "Error", description = f"An unknown error occured. Please contact @{self.bot.ownername}.")
+                embed.color = discord.Colour.red()
+                return embed
         
         # Process data
         if data:
@@ -103,19 +58,34 @@ class apicmds(commands.Cog):
             if cpu_info:
                 ghzs = f"**Base Frequency** {cpu_info['Base Freq.(GHz)']}GHz" if cpu_info['Max. Turbo Freq.(GHz)'] == "N/A" else f"**Base/Max Frequency** {cpu_info['Base Freq.(GHz)']}/{cpu_info['Max. Turbo Freq.(GHz)']}GHz"
                 embed = discord.Embed(title=f"Intel {cpu_info['Product']}", color=0x2494A1, description=f"**Cores/Threads** {cpu_info['Cores']}c{cpu_info['Threads']}t\n**Release Date** {cpu_info['Release Date']}\n**TDP** {cpu_info['TDP(W)']}W\n**Lithography** {cpu_info['Lithography(nm)']}nm\n{ghzs}")
-                embed.set_footer(text=f"Requested by {ctx.author.name} | api.nikolan.xyz/intel-cpu")
-                await ctx.respond(embed=embed)
+                embed.set_footer(text=f"Requested by {ctxauthor} | api.nikolan.xyz/intel-cpu")
+                return embed
             else:
                 embed = discord.Embed(title = "Error", description = f"An error occurred while fetching CPU data from the API. Try again later.")
                 embed.color = discord.Colour.red()
-                await ctx.respond(embed=embed)
-
-    @commands.slash_command(name="amd-cpu", description="Sends a random AMD CPU.")
-    @commands.cooldown(1, 3, BucketType.user)
-    async def cpum(self, ctx):
+                return embed
+        
+    async def amdcpu(self, ctxauthor):
         # Get data from API
         endpoint = "https://api.nikolan.xyz/amd-cpu"
-        cpu_info = await self.fetch_data(ctx, endpoint)
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.get(endpoint, timeout=3) as response:
+                    response.raise_for_status()
+                    if response.status == 200:
+                        cpu_info = await response.json()
+            except aiohttp.ClientError:
+                embed = discord.Embed(title = "Error", description = f"An error occurred while fetching data from the API. Please contact @{self.bot.ownername}.")
+                embed.color = discord.Colour.red()
+                return embed
+            except asyncio.TimeoutError:
+                embed = discord.Embed(title = "Error", description = f"The request to the API timed out. Please contact @{self.bot.ownername}.")
+                embed.color = discord.Colour.red()
+                return embed
+            except Exception:
+                embed = discord.Embed(title = "Error", description = f"An unknown error occured. Please contact @{self.bot.ownername}.")
+                embed.color = discord.Colour.red()
+                return embed
         
         # Process data
         if cpu_info:
@@ -126,7 +96,7 @@ class apicmds(commands.Cog):
             rdate = cpu_info['Launch Date']
             if rdate == "":
                 rdate = "Unknown"
-            if not "AMD" in cpumodel:
+            if not "AMD" in str(cpumodel):
                 cpumodel = f"AMD {cpumodel}"
             tdp = cpu_info['Default TDP']
             if tdp == "":
@@ -141,12 +111,36 @@ class apicmds(commands.Cog):
             **Lithography** {cpu_info['Processor Technology for CPU Cores']}
             **Base/Max Frequency** {clockbase}/{clockmax}GHz""")
 
-            embed.set_footer(text=f"Requested by {ctx.author.name} | api.nikolan.xyz/amd-cpu")
-            await ctx.respond(embed=embed)
+            embed.set_footer(text=f"Requested by {ctxauthor} | api.nikolan.xyz/amd-cpu")
+            return embed
         else:
             embed = discord.Embed(title = "Error", description = f"An error occurred while fetching CPU data from the API. Try again later.")
             embed.color = discord.Colour.red()
-            await ctx.respond(embed=embed)
+            return embed
+
+    @commands.slash_command(name="cpu", description="Sends a random Intel or AMD CPU.")
+    @commands.cooldown(1, 3, BucketType.user)
+    async def random_cpu(self, ctx):
+        await ctx.defer(ephemeral=False)
+        view = discord.ui.View()
+        async def randintel(interaction):
+            the = await self.intelcpu(ctx.author.name)
+            await interaction.respond(embed=the, view=view)
+        async def randamd(interaction):
+            the = await self.amdcpu(ctx.author.name)
+            await interaction.respond(embed=the, view=view)
+        intelb = discord.ui.Button(label="Intel", style=discord.ButtonStyle.blurple)
+        amdb = discord.ui.Button(label="AMD", style=discord.ButtonStyle.red)
+        intelb.callback = randintel
+        amdb.callback = randamd
+        view.add_item(intelb)
+        view.add_item(amdb)
+        if random.choice(["intel", "amd"]) == "intel":
+            the = await self.intelcpu(ctx.author.name)
+            return await ctx.respond(embed=the, view=view)
+        else:
+            the = await self.amdcpu(ctx.author.name)
+            return await ctx.respond(embed=the, view=view)
 
     @commands.slash_command(name="random-gpu", description="Sends a random Nvidia or AMD GPU.")
     @commands.cooldown(1, 3, BucketType.user)
