1c6795b174c68e41f905582d882f148b67b10b05
nikolan123 <94351312+nikolan123@users.noreply.github.com>
2024-08-16 14:41:57 +0300

    ugh

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-
-response = requests.get('https://api.fbi.gov/wanted/v1/list')
-data = json.loads(response.content)
-print(data['total'])
-print(data['items'][0]['title'])
