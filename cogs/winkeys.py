8864428191f287042863c8b07c14a22c278cb640
nikolan123 <94351312+nikolan123@users.noreply.github.com>
2024-08-20 12:41:08 +0300

    office97key and 95oemkey

--- cogs/winkeys.py
+++ cogs/winkeys.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import csv
 import aiofiles
 import random
+from datetime import datetime, timedelta
 
 async def getoss(ctx):
     data = []
@@ -62,6 +63,51 @@ class winkeyss(commands.Cog):
                         embed.set_thumbnail(url="https://64.media.tumblr.com/f0b0786998dc2e44bfe179e9da3fa116/39dad773e2bb50bc-4c/s540x810/3534cc436c2b90f526bd483f632d0ff804a80e7b.gif")
                         return await ctx.respond(embed=embed)
         return await ctx.respond(discord.Embed(color=discord.Color.red(), title="Error", description="Error validating key, please try again"))
+    
+    @commands.slash_command(integration_types={discord.IntegrationType.guild_install,discord.IntegrationType.user_install}, name="95oemkey", description="Generates a Windows 95 OEM Key.")
+    async def nfoemkey(self, ctx):
+        #gen first segment
+        day_of_year = random.randint(1, 366)
+        year_suffix = random.randint(95, 103)
+        first_segment = f"{day_of_year:03d}{year_suffix:02d}"
+
+        year_full = 1900 + year_suffix if year_suffix >= 95 else 2000 + year_suffix
+        date = datetime(year_full, 1, 1) + timedelta(days=day_of_year - 1)
+        readable_date = date.strftime("%d %B %Y")
+
+        #gen 3rd segment
+        valid_last_digit = False
+        while not valid_last_digit:
+            third_segment = str(random.randint(0, 999999)).zfill(6)
+            third_segment = '0' + third_segment
+            digit_sum = sum(int(digit) for digit in third_segment)
+            valid_last_digit = digit_sum % 7 == 0 and int(third_segment[-1]) not in [0, 8, 9]
+        
+        cd_key = f"{first_segment}-OEM-{third_segment}-{''.join(str(random.randint(0, 9)) for _ in range(5))}"
+
+        embed = discord.Embed(title="Windows 95 OEM Key", color=0x2494A1, description=f"{cd_key}\nKey printed on {readable_date}")
+        embed.set_footer(text=f"Requested by {ctx.author.name}")
+        embed.set_thumbnail(url="https://64.media.tumblr.com/f0b0786998dc2e44bfe179e9da3fa116/39dad773e2bb50bc-4c/s540x810/3534cc436c2b90f526bd483f632d0ff804a80e7b.gif")
+        return await ctx.respond(embed=embed)
+
+    @commands.slash_command(integration_types={discord.IntegrationType.guild_install,discord.IntegrationType.user_install}, name="office97key", description="Generates an Office 97 Key.")
+    async def o97key(self, ctx):
+        #gen 1st segment
+        digit3 = random.randint(0, 9)
+        digit4 = (digit3 + random.choice([1, 2])) % 10
+        first_segment=f"{random.randint(0, 9)}{random.randint(0, 9)}{digit3}{digit4}"
+        #gen 2nd segment
+        valid_last_digit = False
+        while not valid_last_digit:
+            second_segment = str(random.randint(0, 999999)).zfill(7)
+            digit_sum = sum(int(digit) for digit in second_segment)
+            valid_last_digit = digit_sum % 7 == 0 and int(second_segment[-1]) not in [0, 8, 9]
+        cd_key = f"{first_segment}-{second_segment}"
+
+        embed = discord.Embed(title="Office 97 Key", color=0x2494A1, description=f"{cd_key}")
+        embed.set_footer(text=f"Requested by {ctx.author.name}")
+        embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Office_95_and_97_logo.svg/800px-Office_95_and_97_logo.svg.png")
+        return await ctx.respond(embed=embed)
 
 def setup(bot):
     bot.add_cog(winkeyss(bot))
